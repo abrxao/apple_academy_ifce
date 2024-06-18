0fa945a9b38c69cca6e1158cfa37bd5aac3be159
Luiz Phelipe Pimenta Frota <luiz.frota@academico.ifrn.edu.br>
2024-06-18 16:20:57 -0300

    :bug: bugfix: corrigindo bug na pagina inicial
    Atualizando o banco de dados

--- BoJogar/BoJogar/UserView.swift
+++ BoJogar/BoJogar/UserView.swift
@@ -10,68 +10,72 @@ import SwiftUI
 // ContentView.swif
 struct UserView: View {
     @EnvironmentObject var userRepo: UserRepo
+    
     var body: some View {
-        ZStack(alignment: .topLeading){
-            
-            Image("courtBg")
-                .resizable()
-                .frame(maxWidth: .infinity)
-                .scaledToFit()
-                .accessibilityHidden(true)
-                .opacity(0.4)
-                .padding(.bottom,256)
-                .background(.primaryBlue)
-
-            
-            ScrollView(showsIndicators: false) {
-                if (userRepo.userData != nil){
-                    VStack(alignment: .leading) {
-                        Spacer()
-                            .frame(height: 40)
-                            .frame(maxWidth: .infinity)
-                        Text("Olá,")
-                            .font(.largeTitle)
-                            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
-                            .foregroundStyle(.white)
-                            .shadow(radius: 4, y:4)
-                            .shadow(color:.gray700, radius: 1)
-                            .multilineTextAlignment(.leading)
-                        
-                        Text(userRepo.userData?.fullName ?? " ")
-                            .font(.largeTitle)
-                            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
-                            .foregroundStyle(.white)
-                            .shadow(radius: 4, y:4)
-                            .shadow(color:.gray700, radius: 1)
-                            .multilineTextAlignment(.leading)
-                        
-                        Text("Que atividade você deseja praticar hoje?")
-                            .shadow(radius: 4, y:4)
-                            .shadow(color:.gray700,radius: 1)
-                            .fontWeight(.semibold)
-                            .foregroundStyle(.white)
+        
+        ScrollView(showsIndicators: false) {
+            ZStack(alignment: .topLeading){
+                
+                Image("courtBg")
+                    .resizable()
+                    .frame(maxWidth: .infinity)
+                    .scaledToFit()
+                    .accessibilityHidden(true)
+                    .opacity(0.4)
+                    .padding(.vertical,64)
+                    .background(.primaryBlue)
+                    .offset(y:-64)
+                
+                VStack{
+                    if (userRepo.userData != nil){
+                        VStack(alignment: .leading) {
+                            Spacer()
+                                .frame(height: 40)
+                                .frame(maxWidth: .infinity)
+                            Text("Olá,")
+                                .font(.largeTitle)
+                                .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                                .foregroundStyle(.white)
+                                .shadow(radius: 4, y:4)
+                                .shadow(color:.gray700, radius: 1)
+                                .multilineTextAlignment(.leading)
+                            
+                            Text(userRepo.userData?.fullName ?? " ")
+                                .font(.largeTitle)
+                                .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                                .foregroundStyle(.white)
+                                .shadow(radius: 4, y:4)
+                                .shadow(color:.gray700, radius: 1)
+                                .multilineTextAlignment(.leading)
+                            
+                            Text("Que atividade você deseja praticar hoje?")
+                                .shadow(radius: 4, y:4)
+                                .shadow(color:.gray700,radius: 1)
+                                .fontWeight(.semibold)
+                                .foregroundStyle(.white)
+                        }
+                        .padding(.horizontal,20)
                     }
-                    .padding(.horizontal,20)
+                    
+                    Spacer()
+                        .frame(height: 22)
+                    
+                    UserEventsView()
+                        .padding(.horizontal,20)
+                        .padding(.vertical,32)
+                        .background(.white)
+                        .clipShape(UnevenRoundedRectangle(
+                            topLeadingRadius: 32,
+                            bottomLeadingRadius: 0,
+                            bottomTrailingRadius: 0,
+                            topTrailingRadius: 32,
+                            style: .continuous))
                 }
-                
-                Spacer()
-                    .frame(height: 22)
-                
-                UserEventsView()
-                    .padding(.horizontal,20)
-                    .padding(.vertical,32)
-                    .background(.white)
-                    .clipShape(UnevenRoundedRectangle(
-                        topLeadingRadius: 32,
-                        bottomLeadingRadius: 0,
-                        bottomTrailingRadius: 0,
-                        topTrailingRadius: 32,
-                        style: .continuous))
             }
             .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+            .background(.white)
         }
         .frame(maxWidth: .infinity)
-        .background(.white)
         .task{
             await userRepo.getUserData()
         }
